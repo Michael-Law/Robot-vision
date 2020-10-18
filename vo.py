5ecaa80396b60753213def9057fd67199727058a
Michael-Law <mlawsan@outlook.com>
2020-10-18 15:22:36 +0400

    Progression

--- vo.py
+++ vo.py
@@ -9,15 +9,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Visual_Odometry:
-    def __init__(self,frames,method,featureNumber):
+    def __init__(self,framesPath,method,featureNumber):
         self.method = method
         self.featureNumber = featureNumber
-        self.frames = frames
+        self.framesPath = framesPath
 
     #capture two sequential image starting from time_n
-    def readimage():
+    @property
+    def readimage(self):
         imglist = []
-        for index,entries in enumerate(os.listdir(path)):
+        for index,entries in enumerate(os.listdir(self.framesPath)):
             if index <time:
                 pass
             elif index >time+1:
@@ -30,76 +31,72 @@ class Visual_Odometry:
         return imglist
 
     #Extracting features and descriptors from the image(t) and image(t+1)
-    def featureExtraction():
+    def featureExtraction(self):
+
+        def Orb_feature_detection():
+            orb = cv2.ORB_create(edgeThreshold=15, patchSize=31, nlevels=8, fastThreshold=20, scaleFactor=1.2, WTA_K=2,scoreType=cv2.ORB_HARRIS_SCORE, firstLevel=0, nfeatures=self.featureNumber)
+            return orb
         
+        def Surf_feature_detection():
+            surf = cv2.SURF(self.featureNumber)
+            return surf
+        
+        def Akaze_feature_detection():
+            kaze = cv2.AKAZE_create(self.featureNumber)
+            return kaze
+        
+        def Fast_feature_detection():
+            fast = cv2.FastFeatureDetector(self.featureNumber)
+            return fast
 
-        def Orb_feature_detection(image_pair):
-            orb = cv2.ORB_create(edgeThreshold=15, patchSize=31, nlevels=8, fastThreshold=20, scaleFactor=1.2, WTA_K=2,scoreType=cv2.ORB_HARRIS_SCORE, firstLevel=0, nfeatures=500)
-            kp0 = orb.detect(image_pair[0],None)
-            kp1 = orb.detect(image_pair[1],None)
-            kp0,des0 = orb.compute(image_pair[0],kp0)
-            kp1,des1 = orb.compute(image_pair[1],kp1)
-            return kp0 ,kp1 ,des0, des1
-
-        def Surf_feature_detection(image_pair):
-            surf = cv2.SURF(500)
-            kp0 = surf.detect(image_pair[0],None)
-            kp1 = surf.detect(image_pair[1],None)
-            kp0,des0 = surf.compute(image_pair[0],kp0)
-            kp1,des1 = surf.compute(image_pair[1],kp1)
-            return kp0 ,kp1 ,des0, des1
-
-        def Akaze_feature_detection(image_pair):
-            kaze = cv2.AKAZE_create(500)
-            kp0 = kaze.detect(image_pair[0],None)
-            kp1 = kaze.detect(image_pair[1],None)
-            kp0,des0 = kaze.compute(image_pair[0],kp0)
-            kp1,des1 = kaze.compute(image_pair[1],kp1)
-            return kp0 ,kp1 ,des0, des1    
-
-        def Fast_feature_detection(image_pair):
-            fast = cv2.FastFeatureDetector(500)
-            kp0 = fast.detect(image_pair[0],None)
-            kp1 = fast.detect(image_pair[1],None)
-            kp0,des0 = fast.compute(image_pair[0],kp0)
-            kp1,des1 = fast.compute(image_pair[1],kp1)
-            return kp0 ,kp1 ,des0, des1    
+        def keypointsANDdescriptors(featureExtraction_method):
+            successivepoints = []
+            for image in self.readimage:
+                keypoint = featureExtraction_method.detect(image,None)
+                keypoint,descriptor = featureExtraction_method.detect(image,keypoint)
+                successivepoints.append(keypoint)
+                successivepoints.append(descriptor)
+            return successivepoints
         
         if self.method == 'Orb':
-            
+            keypointsANDdescriptors(Orb_feature_detection())
+        elif self.method == 'Surf':
+            keypointsANDdescriptors(Surf_feature_detection())
+        elif self.method == 'Kaze':
+            keypointsANDdescriptors(Akaze_feature_detection())
+        elif self.method == 'Fast':
+            keypointsANDdescriptors(Fast_feature_detection())
 
-images = readimage('./rgb',0)   
-kep,kep0,des,des0 = Surf_feature_detection(images)
+    def keypointsMatching(self):
 
 
-FLANN_INDEX_KDTREE = 0
-index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-search_params = dict(checks=50)   # or pass empty dictionary
 
-flann = cv2.FlannBasedMatcher(index_params,search_params)
 
-matches = flann.knnMatch(np.float32(des),np.float32(des0),k=2)
+# FLANN_INDEX_KDTREE = 0
+# index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+# search_params = dict(checks=50)   # or pass empty dictionary
 
-# Need to draw only good matches, so create a mask
-matchesMask = [[0,0] for i in range(len(matches))]
+# flann = cv2.FlannBasedMatcher(index_params,search_params)
 
-pts1 = []
-pts2 = []
+# matches = flann.knnMatch(np.float32(des),np.float32(des0),k=2)
 
-for i,(m,n) in enumerate(matches):
-    if m.distance < 0.7*n.distance:
-        matchesMask[i]=[1,0]
-        pts1.append(kep[m.trainIdx].pt)
-        pts2.append(kep0[m.queryIdx].pt)
+# # Need to draw only good matches, so create a mask
+# matchesMask = [[0,0] for i in range(len(matches))]
 
-pts1 = np.int32(pts1)
-pts2 = np.int32(pts2)
+# pts1 = []
+# pts2 = []
 
-E, mask = cv2.findEssentialMat(pts1 ,pts2, focal=1.0, pp=(0., 0.), method=cv2.RANSAC, prob=0.999, threshold=0.1)
+# for i,(m,n) in enumerate(matches):
+#     if m.distance < 0.7*n.distance:
+#         matchesMask[i]=[1,0]
+#         pts1.append(kep[m.trainIdx].pt)
+#         pts2.append(kep0[m.queryIdx].pt)
 
-print(E)
+# pts1 = np.int32(pts1)
+# pts2 = np.int32(pts2)
 
-points, R, t, mask = cv2.recoverPose(E, pts1, pts2)
+# E, mask = cv2.findEssentialMat(pts1 ,pts2, focal=1.0, pp=(0., 0.), method=cv2.RANSAC, prob=0.999, threshold=0.1)
 
-print(R)
-print(T)
+# print(E)
+
+# points, R, t, mask = cv2.recoverPose(E, pts1, pts2)
