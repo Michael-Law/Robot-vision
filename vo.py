a733659d672efd50c5e0879214f42d6bb38f7b09
Michael-Law <mlawsan@outlook.com>
2020-10-20 03:41:36 +0400

    Loop addition

--- vo.py
+++ vo.py
@@ -6,45 +6,60 @@
 import os
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 class Visual_Odometry:
-    def __init__(self,framesPath,method,featureNumber):
+    def __init__(self, framesPath, method, featureNumber):
         self.method = method
         self.featureNumber = featureNumber
         self.framesPath = framesPath
+        self.time = 0
 
-    #capture two sequential image starting from time_n
+    @property
+    def increment_time(self):
+        self.time += 1
+
+    # capture two sequential image starting from time_n
     @property
     def readimage(self):
         imglist = []
-        for index,entries in enumerate(os.listdir(self.framesPath)):
-            if index <time:
+        for index, entries in enumerate(os.listdir(self.framesPath)):
+            if index < self.time:
                 pass
-            elif index >time+1:
+            elif index > self.time + 1:
                 break
             else:
-                img = cv2.imread(os.path.join(path,entries))
-                img = np.asarray(img[:,:])
+                img = cv2.imread(os.path.join(self.framesPath, entries))
+                img = np.asarray(img[:, :])
                 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                imglist.append(img)   
+                imglist.append(img)
         return imglist
 
-    #Extracting features and descriptors from the image(t) and image(t+1)
+    # Extracting features and descriptors from the image(t) and image(t+1)
+    @property
     def featureExtraction(self):
-
         def Orb_feature_detection():
-            orb = cv2.ORB_create(edgeThreshold=15, patchSize=31, nlevels=8, fastThreshold=20, scaleFactor=1.2, WTA_K=2,scoreType=cv2.ORB_HARRIS_SCORE, firstLevel=0, nfeatures=self.featureNumber)
+            orb = cv2.ORB_create(
+                edgeThreshold=15,
+                patchSize=31,
+                nlevels=8,
+                fastThreshold=20,
+                scaleFactor=1.2,
+                WTA_K=2,
+                scoreType=cv2.ORB_HARRIS_SCORE,
+                firstLevel=0,
+                nfeatures=self.featureNumber,
+            )
             return orb
-        
-        def Surf_feature_detection():
-            surf = cv2.SURF(self.featureNumber)
+
+        def Sift_feature_detection():
+            surf = cv2.SIFT(self.featureNumber)
             return surf
-        
+
         def Akaze_feature_detection():
             kaze = cv2.AKAZE_create(self.featureNumber)
             return kaze
-        
+
         def Fast_feature_detection():
             fast = cv2.FastFeatureDetector(self.featureNumber)
             return fast
@@ -52,51 +67,67 @@ class Visual_Odometry:
         def keypointsANDdescriptors(featureExtraction_method):
             successivepoints = []
             for image in self.readimage:
-                keypoint = featureExtraction_method.detect(image,None)
-                keypoint,descriptor = featureExtraction_method.detect(image,keypoint)
+                keypoint = featureExtraction_method.detect(image, None)
+                keypoint, descriptor = featureExtraction_method.compute(image, keypoint)
                 successivepoints.append(keypoint)
                 successivepoints.append(descriptor)
             return successivepoints
-        
-        if self.method == 'Orb':
-            keypointsANDdescriptors(Orb_feature_detection())
-        elif self.method == 'Surf':
-            keypointsANDdescriptors(Surf_feature_detection())
-        elif self.method == 'Kaze':
-            keypointsANDdescriptors(Akaze_feature_detection())
-        elif self.method == 'Fast':
-            keypointsANDdescriptors(Fast_feature_detection())
 
+        if self.method == "Orb":
+            kep = keypointsANDdescriptors(Orb_feature_detection())
+        elif self.method == "Sift":
+            kep = keypointsANDdescriptors(Sift_feature_detection())
+        elif self.method == "Kaze":
+            kep = keypointsANDdescriptors(Akaze_feature_detection())
+        elif self.method == "Fast":
+            kep = keypointsANDdescriptors(Fast_feature_detection())
+
+        return kep
+
+    @property
     def keypointsMatching(self):
+        matcher = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_FLANNBASED)
+        knn_matches = matcher.knnMatch(
+            np.float32(self.featureExtraction[3]),
+            np.float32(self.featureExtraction[1]),
+            2,
+        )
+
+        pts0 = []
+        pts1 = []
+
+        for i, (m, n) in enumerate(knn_matches):
+            if m.distance < 0.7 * n.distance:
+                # matchesMask[i] = [1, 0]
+                pts0.append(self.featureExtraction[0][m.trainIdx].pt)
+                pts1.append(self.featureExtraction[2][n.queryIdx].pt)
+
+        return np.int32(pts0), np.int32(pts1)
+
+    @property
+    def EssentialMatrix(self):
+        pts1, pts2 = self.keypointsMatching
+        E, mask = cv2.findEssentialMat(
+            pts1,
+            pts2,
+            focal=1.0,
+            pp=(0.0, 0.0),
+            method=cv2.RANSAC,
+            prob=0.999,
+            threshold=0.001,
+        )
+        return E, mask
+
+    def RotationalAndTranslational(self):
+        pts1, pts2 = self.keypointsMatching
+        E, mask = self.EssentialMatrix
+        for __ in os.listdir(self.framesPath):
+            __, R, t, __ = cv2.recoverPose(E, pts1, pts2)
+            self.increment_time
+            yield R, t
 
 
+vo = Visual_Odometry("./data_1", "Orb", 500)
 
-
-# FLANN_INDEX_KDTREE = 0
-# index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-# search_params = dict(checks=50)   # or pass empty dictionary
-
-# flann = cv2.FlannBasedMatcher(index_params,search_params)
-
-# matches = flann.knnMatch(np.float32(des),np.float32(des0),k=2)
-
-# # Need to draw only good matches, so create a mask
-# matchesMask = [[0,0] for i in range(len(matches))]
-
-# pts1 = []
-# pts2 = []
-
-# for i,(m,n) in enumerate(matches):
-#     if m.distance < 0.7*n.distance:
-#         matchesMask[i]=[1,0]
-#         pts1.append(kep[m.trainIdx].pt)
-#         pts2.append(kep0[m.queryIdx].pt)
-
-# pts1 = np.int32(pts1)
-# pts2 = np.int32(pts2)
-
-# E, mask = cv2.findEssentialMat(pts1 ,pts2, focal=1.0, pp=(0., 0.), method=cv2.RANSAC, prob=0.999, threshold=0.1)
-
-# print(E)
-
-# points, R, t, mask = cv2.recoverPose(E, pts1, pts2)
+for Rotational, Translational in vo.RotationalAndTranslational():
+    print(Rotational)
